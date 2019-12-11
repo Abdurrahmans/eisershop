719c0326205f79c9d867d1d4ee38d46891e26232
Abdurrahmans <abdurrahmanswe1997@gmail.com>
2019-12-11 23:03:30 +0600

    Done

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -66,28 +66,28 @@
 <!-- Logout Modal-->
 
 <!-- Bootstrap core JavaScript-->
-<script src="{{asset('/public/admin')}}/vendor/jquery/jquery.min.js"></script>
-<script src="{{asset('/public/admin')}}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{secure_asset('/public/admin')}}/vendor/jquery/jquery.min.js"></script>
+<script src="{{secure_asset('/public/admin')}}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="{{asset('/public/admin')}}/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="{{secure_asset('/public/admin')}}/vendor/jquery-easing/jquery.easing.min.js"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="{{asset('/public/admin')}}/js/sb-admin-2.min.js"></script>
+<script src="{{secure_asset('/public/admin')}}/js/sb-admin-2.min.js"></script>
 <!-- Page level plugins -->
-<script src="{{asset('/public/admin')}}/vendor/datatables/jquery.dataTables.min.js"></script>
-<script src="{{asset('/public/admin')}}/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+<script src="{{secure_asset('/public/admin')}}/vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="{{secure_asset('/public/admin')}}/vendor/datatables/dataTables.bootstrap4.min.js"></script>
 <!-- Page level custom scripts -->
 
-<script src="{{asset('/public/admin')}}/vendor/chart.js/Chart.min.js"></script>
+<script src="{{secure_asset('/public/admin')}}/vendor/chart.js/Chart.min.js"></script>
 
 
-<script src="{{asset('/public/admin')}}/js/demo/chart-area-demo.js"></script>
-<script src="{{asset('/public/admin')}}/js/demo/chart-pie-demo.js"></script>
+<script src="{{secure_asset('/public/admin')}}/js/demo/chart-area-demo.js"></script>
+<script src="{{secure_asset('/public/admin')}}/js/demo/chart-pie-demo.js"></script>
 
-<script src="{{asset('/public/admin')}}/js/demo/datatables-demo.js"></script>
-<script src="{{asset('/public/admin')}}/ckeditor/ckeditor.js"></script>
-<script src="{{asset('/public/admin')}}/ckeditor/samples/js/sample.js"></script>
+<script src="{{secure_asset('/public/admin')}}/js/demo/datatables-demo.js"></script>
+<script src="{{secure_asset('/public/admin')}}/ckeditor/ckeditor.js"></script>
+<script src="{{secure_asset('/public/admin')}}/ckeditor/samples/js/sample.js"></script>
 <script>
     initSample();
 </script>
--- resources/views/front-end/master.blade.php
+++ resources/views/front-end/master.blade.php
@@ -5,22 +5,22 @@
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <link rel="icon" href="{{asset('/public/front')}}/img/favicon.png" type="image/png" />
+    <link rel="icon" href="{{secure_asset('/public/front')}}/img/favicon.png" type="image/png" />
     <title>@yield('title')</title>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{asset('/public/front')}}/css/bootstrap.css" />
-    <link rel="stylesheet" href="{{asset('/public/front')}}/vendors/linericon/style.css" />
-    <link rel="stylesheet" href="{{asset('/public/front')}}/css/font-awesome.min.css" />
-    <link rel="stylesheet" href="{{asset('/public/front')}}/css/themify-icons.css" />
-    <link rel="stylesheet" href="{{asset('/public/front')}}/css/flaticon.css" />
-    <link rel="stylesheet" href="{{asset('/public/front')}}/vendors/owl-carousel/owl.carousel.min.css" />
-    <link rel="stylesheet" href="{{asset('/public/front')}}/vendors/lightbox/simpleLightbox.css" />
-    <link rel="stylesheet" href="{{asset('/public/front')}}/vendors/nice-select/css/nice-select.css" />
-    <link rel="stylesheet" href="{{asset('/public/front')}}/vendors/animate-css/animate.css" />
-    <link rel="stylesheet" href="{{asset('/public/front')}}/vendors/jquery-ui/jquery-ui.css" />
+    <link rel="stylesheet" href="{{secure_asset('/public/front')}}/css/bootstrap.css" />
+    <link rel="stylesheet" href="{{secure_asset('/public/front')}}/vendors/linericon/style.css" />
+    <link rel="stylesheet" href="{{secure_asset('/public/front')}}/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="{{secure_asset('/public/front')}}/css/themify-icons.css" />
+    <link rel="stylesheet" href="{{secure_asset('/public/front')}}/css/flaticon.css" />
+    <link rel="stylesheet" href="{{secure_asset('/public/front')}}/vendors/owl-carousel/owl.carousel.min.css" />
+    <link rel="stylesheet" href="{{secure_asset('/public/front')}}/vendors/lightbox/simpleLightbox.css" />
+    <link rel="stylesheet" href="{{secure_asset('/public/front')}}/vendors/nice-select/css/nice-select.css" />
+    <link rel="stylesheet" href="{{secure_asset('/public/front')}}/vendors/animate-css/animate.css" />
+    <link rel="stylesheet" href="{{secure_asset('/public/front')}}/vendors/jquery-ui/jquery-ui.css" />
     <!-- main css -->
-    <link rel="stylesheet" href="{{asset('/public/front')}}/css/style.css" />
-    <link rel="stylesheet" href="{{asset('/public/front')}}/css/responsive.css" />
+    <link rel="stylesheet" href="{{secure_asset('/public/front')}}/css/style.css" />
+    <link rel="stylesheet" href="{{secure_asset('/public/front')}}/css/responsive.css" />
 </head>
 
 <body>
@@ -36,20 +36,20 @@
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="{{asset('/public/front')}}/js/jquery-3.2.1.min.js"></script>
-<script src="{{asset('/public/front')}}/js/popper.js"></script>
-<script src="{{asset('/public/front')}}/js/bootstrap.min.js"></script>
-<script src="{{asset('/public/front')}}/js/stellar.js"></script>
-<script src="{{asset('/public/front')}}/vendors/lightbox/simpleLightbox.min.js"></script>
-<script src="{{asset('/public/front')}}/vendors/nice-select/js/jquery.nice-select.min.js"></script>
-<script src="{{asset('/public/front')}}/vendors/isotope/imagesloaded.pkgd.min.js"></script>
-<script src="{{asset('/public/front')}}/vendors/isotope/isotope-min.js"></script>
-<script src="{{asset('/public/front')}}/vendors/owl-carousel/owl.carousel.min.js"></script>
-<script src="{{asset('/public/front')}}/js/jquery.ajaxchimp.min.js"></script>
-<script src="{{asset('/public/front')}}/vendors/counter-up/jquery.waypoints.min.js"></script>
-<script src="{{asset('/public/front')}}/vendors/counter-up/jquery.counterup.js"></script>
-<script src="{{asset('/public/front')}}/js/mail-script.js"></script>
-<script src="{{asset('/public/front')}}/js/theme.js"></script>
+<script src="{{secure_asset('/public/front')}}/js/jquery-3.2.1.min.js"></script>
+<script src="{{secure_asset('/public/front')}}/js/popper.js"></script>
+<script src="{{secure_asset('/public/front')}}/js/bootstrap.min.js"></script>
+<script src="{{secure_asset('/public/front')}}/js/stellar.js"></script>
+<script src="{{secure_asset('/public/front')}}/vendors/lightbox/simpleLightbox.min.js"></script>
+<script src="{{secure_asset('/public/front')}}/vendors/nice-select/js/jquery.nice-select.min.js"></script>
+<script src="{{secure_asset('/public/front')}}/vendors/isotope/imagesloaded.pkgd.min.js"></script>
+<script src="{{secure_asset('/public/front')}}/vendors/isotope/isotope-min.js"></script>
+<script src="{{secure_asset('/public/front')}}/vendors/owl-carousel/owl.carousel.min.js"></script>
+<script src="{{secure_asset('/public/front')}}/js/jquery.ajaxchimp.min.js"></script>
+<script src="{{secure_asset('/public/front')}}/vendors/counter-up/jquery.waypoints.min.js"></script>
+<script src="{{secure_asset('/public/front')}}/vendors/counter-up/jquery.counterup.js"></script>
+<script src="{{secure_asset('/public/front')}}/js/mail-script.js"></script>
+<script src="{{secure_asset('/public/front')}}/js/theme.js"></script>
 </body>
 
 </html>
