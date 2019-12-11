6307f4fb48a28590209ac2230c21f6c73003826d
Abdurrahmans <abdurrahmanswe1997@gmail.com>
2019-12-11 22:57:47 +0600

    done

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -12,14 +12,14 @@
     <title>@yield('title')</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{asset('/public/admin')}}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="{{secure_asset('/public/admin')}}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="{{asset('/public/admin')}}/css/sb-admin-2.min.css" rel="stylesheet">
-    <link href="{{asset('/public/admin')}}/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('/public/admin')}}/ckeditor/sample/css/samples.css">
-    <link rel="stylesheet" href="{{asset('/public/admin')}}/ckeditor/sample/toolbarconfigurator/lib/codemirror/neo.css">
+    <link href="{{secure_asset('/public/admin')}}/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="{{secure_asset('/public/admin')}}/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{secure_asset('/public/admin')}}/ckeditor/sample/css/samples.css">
+    <link rel="stylesheet" href="{{secure_asset('/public/admin')}}/ckeditor/sample/toolbarconfigurator/lib/codemirror/neo.css">
 </head>
 
 <body id="page-top">
